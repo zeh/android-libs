0848647302186a0851882005d1eba3d96d09db86
Zeh Fernando <zeh@zehfernando.com>
2012-10-05 10:39:06 -0400

    New, easier logger

---
+++ com/zehfernando/utils/F.java
@@ -0,0 +1,53 @@
+package com.zehfernando.utils;
+
+import android.util.Log;
+
+// Ugh... global functions. static imports are not automatic
+// This is ugly, need to refactor
+
+public class F {
+
+	public static void log() {
+		StackTraceElement[] stack = Thread.currentThread().getStackTrace();
+
+		String[] classNames = stack[3].getClassName().split("\\.");
+		String className = classNames[classNames.length - 1];
+		String methodName = stack[3].getMethodName();
+
+		String tag = className+"."+methodName+"()";
+		Log.v(tag, "-");
+	}
+
+	public static void log(String __message) {
+		StackTraceElement[] stack = Thread.currentThread().getStackTrace();
+
+		String[] classNames = stack[3].getClassName().split("\\.");
+		String className = classNames[classNames.length - 1];
+		String methodName = stack[3].getMethodName();
+
+		String tag = className+"."+methodName+"()";
+		Log.v(tag, __message);
+	}
+
+	public static void warn(String __message) {
+		StackTraceElement[] stack = Thread.currentThread().getStackTrace();
+
+		String[] classNames = stack[3].getClassName().split("\\.");
+		String className = classNames[classNames.length - 1];
+		String methodName = stack[3].getMethodName();
+
+		String tag = className+"."+methodName+"()";
+		Log.w(tag, __message);
+	}
+
+	public static void error(String __message) {
+		StackTraceElement[] stack = Thread.currentThread().getStackTrace();
+
+		String[] classNames = stack[3].getClassName().split("\\.");
+		String className = classNames[classNames.length - 1];
+		String methodName = stack[3].getMethodName();
+
+		String tag = className+"."+methodName+"()";
+		Log.e(tag, __message);
+	}
+}
