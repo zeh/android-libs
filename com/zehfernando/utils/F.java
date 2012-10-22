93cd3cf93bb015f1015dd8816dd9c063bf99c901
Zeh Fernando <zeh@zehfernando.com>
2012-10-22 15:12:41 -0400

    Added an info() log method

--- com/zehfernando/utils/F.java
+++ com/zehfernando/utils/F.java
@@ -29,6 +29,17 @@ public class F {
 		Log.v(tag, __message);
 	}
 
+	public static void info(String __message) {
+		StackTraceElement[] stack = Thread.currentThread().getStackTrace();
+
+		String[] classNames = stack[3].getClassName().split("\\.");
+		String className = classNames[classNames.length - 1];
+		String methodName = stack[3].getMethodName();
+
+		String tag = className+"."+methodName+"()";
+		Log.i(tag, __message);
+	}
+
 	public static void warn(String __message) {
 		StackTraceElement[] stack = Thread.currentThread().getStackTrace();
 
