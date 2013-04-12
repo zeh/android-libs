b9b6359da969044e6d81f4122be0768784b40a0f
Zeh Fernando <zeh@zehfernando.com>
2013-04-12 16:43:03 -0400

    Added a description, methods for safe scrolling, listeners, and getters

--- com/zehfernando/display/widgets/ZScrollView.java
+++ com/zehfernando/display/widgets/ZScrollView.java
@@ -2,76 +2,113 @@ package com.zehfernando.display.widgets;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.view.MotionEvent;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.widget.ScrollView;
-
-import com.zehfernando.utils.F;
-
-// Only scrolls vertically, allowing horizontal scroll events to be passed to children
-// http://stackoverflow.com/questions/2646028/android-horizontalscrollview-within-scrollview-touch-handling
-
-// TODO: don't use this. Not working.
 
 public class ZScrollView extends ScrollView {
 
-	private float xDistance, yDistance, lastX, lastY;
+	/*
+	 * A better ScrollView, based on my previous ZHorizontalScrollView
+	 * . Adds a setScrollViewListener() method for capturing scrolling events
+	 * . Adds getMaxScrollY()
+	 * . Adds getPosition() (0-1)
+	 * . Adds a scrollToWithGuarantees() that scrolls even if you're trying to scroll to a position that is not available anymore (because a child object was just added)
+	 */
 
-	public ZScrollView(Context context, AttributeSet attrs) {
-		super(context, attrs);
+	// Properties
+	private int desiredScrollX = -1;
+	private int desiredScrollY = -1;
+
+	// Instances
+	private OnGlobalLayoutListener gol;
+
+	private ZScrollViewListener scrollViewListener = null;
+
+
+	// ================================================================================================================
+	// CONSTRUCTOR ----------------------------------------------------------------------------------------------------
+
+	public ZScrollView(Context __context) {
+		super(__context);
 	}
+
+	public ZScrollView(Context __context, AttributeSet __attrs) {
+		super(__context, __attrs);
+	}
+
+	public ZScrollView(Context __context, AttributeSet __attrs, int __defStyle) {
+		super(__context, __attrs, __defStyle);
+	}
+
+	// ================================================================================================================
+	// EVENT INTERFACE ------------------------------------------------------------------------------------------------
 
 	@Override
-	public boolean onInterceptTouchEvent(MotionEvent ev) {
-		F.log("intercepting...");
-		switch (ev.getAction()) {
-			case MotionEvent.ACTION_DOWN:
-				xDistance = yDistance = 0f;
-				lastX = ev.getX();
-				lastY = ev.getY();
-				break;
-			case MotionEvent.ACTION_MOVE:
-				final float curX = ev.getX();
-				final float curY = ev.getY();
-				xDistance += Math.abs(curX - lastX);
-				yDistance += Math.abs(curY - lastY);
-				lastX = curX;
-				lastY = curY;
-
-				F.log("xDistance = " + xDistance + ", yDistance = " + yDistance);
-				if (xDistance > yDistance) return false;
-		}
-
-		return super.onInterceptTouchEvent(ev);
+	protected void onScrollChanged(int __x, int __y, int __oldx, int __oldy) {
+		super.onScrollChanged(__x, __y, __oldx, __oldy);
+		//F.log("Scrolling from " + __oldy + " to " + __y);
+		if (scrollViewListener != null) scrollViewListener.onScrollChanged(this, __x, __y, __oldx, __oldy);
 	}
-}
 
-/*ScrollView {
-private final GestureDetector mGestureDetector;
-View.OnTouchListener mGestureListener;
+	// ================================================================================================================
+	// PUBLIC INTERFACE -----------------------------------------------------------------------------------------------
 
-public ZScrollView(Context context, AttributeSet attrs) {
-	super(context, attrs);
-	mGestureDetector = new GestureDetector(new YScrollDetector());
-	setFadingEdgeLength(0);
-}
-
-@Override
-public boolean onInterceptTouchEvent(MotionEvent ev) {
-	F.log("intercepting...");
-	return super.onInterceptTouchEvent(ev) && mGestureDetector.onTouchEvent(ev);
-}
-
-// Return false if we're scrolling in the x direction
-class YScrollDetector extends SimpleOnGestureListener {
-	@Override
-	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-		F.log("distance X = " + distanceX + ", distanceY = " + distanceY);
-		if(Math.abs(distanceY) > Math.abs(distanceX)) {
-			F.log("  X is higher");
-			return true;
-		}
-		F.log("  X is lower, doesn't intercept");
-		return false;
+	public void setOnScrollViewListener(ZScrollViewListener __scrollViewListener) {
+		scrollViewListener = __scrollViewListener;
 	}
+
+	public interface ZScrollViewListener {
+		public void onScrollChanged(ZScrollView scrollView, int x, int y, int oldx, int oldy);
+	}
+
+	public void scrollToWithGuarantees(int __x, int __y) {
+		// REALLY Scrolls to a position
+		// When adding items to a scrollView, you can't immediately scroll to it - it takes a while
+		// for the new addition to cycle back and update the scrollView's max scroll... so we have
+		// to wait and re-set as necessary
+
+		scrollTo(__x, __y);
+
+		desiredScrollX = -1;
+		desiredScrollY = -1;
+
+		if (getScrollX() != __x || getScrollY() != __y) {
+			// Didn't scroll properly: will create an event to try scrolling again later
+
+			if (getScrollX() != __x) desiredScrollX = __x;
+			if (getScrollY() != __y) desiredScrollY = __y;
+
+			if (gol == null) {
+				gol = new OnGlobalLayoutListener() {
+					@Override
+					public void onGlobalLayout() {
+
+						int nx = desiredScrollX == -1 ? getScrollX() : desiredScrollX;
+						int ny = desiredScrollY == -1 ? getScrollY() : desiredScrollY;
+
+						desiredScrollX = -1;
+						desiredScrollY = -1;
+
+						scrollTo(nx, ny);
+					}
+				};
+
+				getViewTreeObserver().addOnGlobalLayoutListener(gol);
+			}
+		}
+	}
+
+
+	// ================================================================================================================
+	// ACCESSOR INTERFACE ---------------------------------------------------------------------------------------------
+
+	public int getMaxScrollY() {
+		return getChildAt(0).getHeight() - getHeight();
+	}
+
+	public float getPositionY() {
+		if (getMaxScrollY() == 0) return 0;
+		return (float) getScrollY() / getMaxScrollY();
+	}
+
 }
-}*/
